5717782dfd3bbfa29cd8b1d584909e884de041cb
Elton Viana <eltonvs@outlook.com>
2017-10-19 13:21:30 -0300

    Remove ActionBar from Login screen

--- app/src/main/java/com/eltonviana/behere/LoginActivity.java
+++ app/src/main/java/com/eltonviana/behere/LoginActivity.java
@@ -3,21 +3,19 @@ package com.eltonviana.behere;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -90,6 +88,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+
+        getSupportActionBar().hide();
     }
 
     private void populateAutoComplete() {
