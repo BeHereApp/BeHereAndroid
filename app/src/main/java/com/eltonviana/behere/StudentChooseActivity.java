479e5e5b1b39d51e2ac6ab218789b8b922ba45b8
Thais Lins <thaisfli@hotmail.com>
2017-10-24 23:40:28 -0300

    add new activities

---
+++ app/src/main/java/com/eltonviana/behere/StudentChooseActivity.java
@@ -0,0 +1,27 @@
+package com.eltonviana.behere;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+public class StudentChooseActivity extends CustomActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_choose);
+
+    }
+
+    public void chooseQRCode(View v) {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(camera);
+    }
+
+    public void choosePassword(View v) {
+        Intent intent = new Intent(StudentChooseActivity.this, StudentPasswordActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/eltonviana/behere/StudentPasswordActivity.java
@@ -0,0 +1,37 @@
+package com.eltonviana.behere;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class StudentPasswordActivity extends CustomActivity {
+
+    private EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_password);
+
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        //adds back arrow to layout
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void enterPassword(View v) {
+        String userPassword = etPassword.getText().toString();
+        etPassword.setText("");
+    }
+}
