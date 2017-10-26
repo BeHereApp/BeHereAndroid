4ed0bea4b7ffeaf39e38544296cf2afc0938a32c
Thais Lins <thaisfli@hotmail.com>
2017-10-26 16:44:22 -0300

    Create Student Result Activity

---
+++ app/src/main/java/br/ufrn/imd/behere/StudentResultActivity.java
@@ -0,0 +1,62 @@
+package br.ufrn.imd.behere;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class StudentResultActivity extends AppCompatActivity {
+
+    public static final String PASSWORD_RESULT = "password_result";
+    private ImageView imgResult;
+    private TextView txtResult;
+    private Button btnResult;
+    private boolean resultSuccess;
+    private String result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_result);
+        setup(savedInstanceState);
+    }
+
+    public void setup(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        result = intent.getStringExtra(PASSWORD_RESULT);
+
+        imgResult = (ImageView) findViewById(R.id.imgResult);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        btnResult = (Button) findViewById(R.id.btnResult);
+        checkResult();
+    }
+
+    public void checkResult() {
+        if (result.equals("password")) {
+            resultSuccess = true;
+            imgResult.setImageResource(R.drawable.positive_result);
+            txtResult.setText("Success");
+            btnResult.setText("Ok");
+        } else {
+            resultSuccess = false;
+            imgResult.setImageResource(R.drawable.negative_result);
+            txtResult.setText("Failed");
+            btnResult.setText("Try Again");
+        }
+    }
+
+    public void performResultAction(View v) {
+        Intent intent;
+        if (resultSuccess == true) {
+            intent = new Intent(this, StudentChooseActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        } else {
+            intent = new Intent(this, StudentPasswordActivity.class);
+        }
+        startActivity(intent);
+        finish();
+    }
+}
