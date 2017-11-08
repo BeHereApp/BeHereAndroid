9650ac4d880a81bbbfd83aba273bbc2b9d446fb7
Thais Lins <thaisfli@hotmail.com>
2017-11-08 14:53:18 -0300

    verify login with database

--- app/src/main/java/br/ufrn/imd/behere/activities/LoginActivity.java
+++ app/src/main/java/br/ufrn/imd/behere/activities/LoginActivity.java
@@ -1,22 +1,60 @@
 package br.ufrn.imd.behere.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
 
 import br.ufrn.imd.behere.R;
+import br.ufrn.imd.behere.utils.DatabaseInstance;
 
 public class LoginActivity extends CustomActivity {
+
+    private EditText txtUsername;
+    private EditText txtPassword;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setup();
+    }
+
+    public void setup() {
+        DatabaseInstance.createDBInstance(getApplicationContext());
+
+        txtUsername = (EditText) findViewById(R.id.username);
+        txtPassword = (EditText) findViewById(R.id.password);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
     }
 
     public void signIn(View v) {
         Intent intent = new Intent(LoginActivity.this, LinkActivity.class);
-        startActivity(intent);
+        Cursor result = DatabaseInstance.database.rawQuery("SELECT id, name, password FROM users", null);
+
+        String inputUsername = txtUsername.getText().toString();
+        String inputPassword = txtPassword.getText().toString();
+        txtUsername.setText("");
+        txtPassword.setText("");
+
+        while (result.moveToNext()) {
+            int id = result.getInt(0);
+            String username = result.getString(1);
+            String password = result.getString(2);
+
+            if (inputUsername.equals(username) && inputPassword.equals(password)) {
+                startActivity(intent);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putInt("logged_user_id", id);
+                editor.apply();
+            }
+        }
+        result.close();
     }
 }
 
--- app/src/main/java/br/ufrn/imd/behere/activities/ProfessorSubjectActivity.java
+++ app/src/main/java/br/ufrn/imd/behere/activities/ProfessorSubjectActivity.java
@@ -2,7 +2,6 @@ package br.ufrn.imd.behere.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
