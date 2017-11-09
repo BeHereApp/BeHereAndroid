a3c0b012e4327122b2a9e39e2a725da14494dffc
Thais Lins <thaisfli@hotmail.com>
2017-11-09 00:48:06 -0300

    Create professor result activity

--- app/src/main/java/br/ufrn/imd/behere/activities/ProfessorPasswordActivity.java
+++ app/src/main/java/br/ufrn/imd/behere/activities/ProfessorPasswordActivity.java
@@ -1,5 +1,6 @@
 package br.ufrn.imd.behere.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,15 +28,15 @@ public class ProfessorPasswordActivity extends CustomActivity {
     }
 
     public void setPassword(View v) {
-        final String password = etPassword.getText().toString();
-        final Integer timeout = Integer.parseInt(etTimeout.getText().toString());
+        String password = etPassword.getText().toString();
+        Integer timeout = Integer.parseInt(etTimeout.getText().toString());
         etPassword.setText("");
         etTimeout.setText("");
 
-        // Intent intent = new Intent(this, ProfessorResultActivity.class);
-        // intent.putExtra(ProfessorResultActivity.PASSWORD_RESULT, password);
-        // intent.putExtra(ProfessorResultActivity.TIMEOUT_RESULT, timeout);
-        // startActivity(intent);
-        // finish();
+        Intent intent = new Intent(this, ProfessorResultActivity.class);
+        intent.putExtra(ProfessorResultActivity.PASSWORD_RESULT, password);
+        intent.putExtra(ProfessorResultActivity.TIMEOUT_RESULT, timeout);
+        startActivity(intent);
+        finish();
     }
 }
